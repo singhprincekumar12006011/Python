6828c4bc2927f787e6578bb1c04a3d66aaff0c0c
Prince Kumar <110832286+singhprincekumar12006011@users.noreply.github.com>
2023-10-27 01:23:34 +0530

    initial commit

---
+++ if_demo2.py
@@ -0,0 +1,9 @@
+# Wap to chech the user is able to vote or not. 
+#take the age from user as an input. and if the age is greater then 18 print the massege that you are eligible to vote.
+
+x=int(input("Enter the age :\n"))
+
+if x >= 18:
+    print("you are eligible for vote :\n")
+else:
+    print("you are not eligible for vote:\n")
